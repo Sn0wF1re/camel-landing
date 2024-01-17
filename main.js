252059186c8e68cdac1e18c9488bb7a1bcd96f3c
Sn0wF1re <mokayaclinton34@gmail.com>
2024-01-18 01:03:44 +0300

    Make changes to page

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+addEventListener("DOMContentLoaded", () => {
+  window.helioCheckout(
+    document.getElementById("helioCheckoutContainer"),
+    {
+      paylinkId: "65a81a9eb1dfe4236081b65a",
+      network: "test",
+      display: "button",
+      onSuccess: event => console.log(event),
+      onError: event => console.log(event),
+      onPending: event => console.log(event),
+      onCancel: () => console.log("Cancelled payment"),
+      onStartPayment: () => console.log("Starting payment"),
+    }
+  );
+});
