7db59d5343c54232e36ce6f587ec47267f9f810a
Sn0wF1re <mokayaclinton34@gmail.com>
2024-01-20 10:44:41 +0300

    Comment out widget implementation

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ addEventListener("DOMContentLoaded", () => {
     document.getElementById("helioCheckoutContainer"),
     {
       paylinkId: "65a81a9eb1dfe4236081b65a",
+      theme: {"colors":{"primaryButtonBackground":"001F3F","primaryButtonText":"F8A235"}},
+      customTexts: {
+        mainButtonTitle: "BUY HUMP",
+      },
       network: "test",
       display: "button",
       onSuccess: event => console.log(event),
